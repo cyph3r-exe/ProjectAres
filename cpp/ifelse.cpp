6e57704a29e062fa7f82c04261e4b42219abdee5
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2024-02-01 23:05:01 +0530

    check kar

---
+++ cpp/ifelse.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    cout << "Enter the calculation you want to carry out: ";
+}
---
+++ cpp/input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter any integer: ";
+    cin >> x;
+    cout << "The integer entered is: " << x << endl;
+    return 0;
+}
